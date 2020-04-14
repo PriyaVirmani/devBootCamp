53e872dcf5644f31792f991cdcd22add2be068b5
Priya Virmani <priyavirmani@Priyas-MacBook-Pro.local>
2020-04-14 10:13:09 +0530

    Refactor changes

--- EvolutionOfTrust/src/main/java/RuleEngine.java
+++ EvolutionOfTrust/src/main/java/RuleEngine.java
@@ -1,24 +1,23 @@
 
 
-public class Players {
-    private final static int CHEAT=0;
-    private final static int COOPERATE=1;
+public class RuleEngine {
+
     private  int playerScoreBoard[] = new int[2];
     private final static int POINTS_WHEN_ONE_COOP = 1;
     private final static int POINTS_WHEN_ONE_CHEAT = 3;
     private final static int POINTS_WHEN_BOTH_COOP =2;
-    public void calculateScoreBasedOnMOve(int playerOneMove , int playerTwoMove) {
+    public void calculateScoreBasedOnMOve(PlayerMove playerOneMove , PlayerMove playerTwoMove) {
 
-         if (playerOneMove==COOPERATE && playerTwoMove == CHEAT){
+         if (playerOneMove==PlayerMove.COOPERATE && playerTwoMove == PlayerMove.CHEAT){
                  playerScoreBoard[0] = playerScoreBoard[0] - POINTS_WHEN_ONE_COOP;
                  playerScoreBoard[1] = playerScoreBoard[1] + POINTS_WHEN_ONE_CHEAT;
         }
-         else if(playerOneMove==CHEAT && playerTwoMove == COOPERATE){
+         else if(playerOneMove==PlayerMove.CHEAT && playerTwoMove == PlayerMove.COOPERATE){
              playerScoreBoard[0] = playerScoreBoard[0] + POINTS_WHEN_ONE_CHEAT;
              playerScoreBoard[1] = playerScoreBoard[1] - POINTS_WHEN_ONE_COOP;
          }
 
-        else if (playerOneMove==COOPERATE && playerTwoMove == COOPERATE){
+        else if (playerOneMove==PlayerMove.COOPERATE && playerTwoMove == PlayerMove.COOPERATE){
              playerScoreBoard[0] = playerScoreBoard[0] + POINTS_WHEN_BOTH_COOP;
              playerScoreBoard[1] = playerScoreBoard[1] + POINTS_WHEN_BOTH_COOP;
         }
--- EvolutionOfTrust/src/test/java/EvalutionOfTrustTest.java
+++ EvolutionOfTrust/src/test/java/EvalutionOfTrustTest.java
@@ -3,47 +3,47 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class EvalutionOfTrustTest {
-    private int CHEAT=0;
-    private int COOPERATE=1;
-    Players player =new Players();
+
+    RuleEngine ruleEngine =new RuleEngine();
     @Test
     public void ifPlayerOneCooperatePlayerTwoCheat(){
-        player.calculateScoreBasedOnMOve(COOPERATE,CHEAT);
-        int [] scores = player.getScoreBoard();
+        ruleEngine.calculateScoreBasedOnMOve(PlayerMove.COOPERATE,PlayerMove.CHEAT);
+        int [] scores = ruleEngine.getScoreBoard();
         assertEquals(-1,scores[0]);
         assertEquals(3,scores[1]);
 
     }
     @Test
     public void ifPlayerOneCheatPlayerTwoCooperate(){
-        player.calculateScoreBasedOnMOve(CHEAT,COOPERATE);
-        int [] scores = player.getScoreBoard();
+        ruleEngine.calculateScoreBasedOnMOve(PlayerMove.CHEAT,PlayerMove.COOPERATE);
+        int [] scores = ruleEngine.getScoreBoard();
        assertEquals(3,scores[0]);
         assertEquals(-1,scores[1]);
     }
     @Test
     public void ifBothPlayersCheat(){
-        player.calculateScoreBasedOnMOve(CHEAT,CHEAT);
-        int [] scores = player.getScoreBoard();
+        ruleEngine.calculateScoreBasedOnMOve(PlayerMove.CHEAT,PlayerMove.CHEAT);
+        int [] scores = ruleEngine.getScoreBoard();
         assertEquals(0,scores[0]);
+
         assertEquals(0,scores[1]);
 
     }
     @Test
     public void ifBothPlayersCooperate(){
-        player.calculateScoreBasedOnMOve(COOPERATE,COOPERATE);
-        int [] scores = player.getScoreBoard();
+        ruleEngine.calculateScoreBasedOnMOve(PlayerMove.COOPERATE,PlayerMove.COOPERATE);
+        int [] scores = ruleEngine.getScoreBoard();
         assertEquals(2,scores[0]);
         assertEquals(2,scores[1]);
     }
 
     @Test
     public void afterFourMovesOfEachPlayer(){
-        player.calculateScoreBasedOnMOve(COOPERATE,COOPERATE);
-        player.calculateScoreBasedOnMOve(COOPERATE,CHEAT);
-        player.calculateScoreBasedOnMOve(CHEAT,COOPERATE);
-        player.calculateScoreBasedOnMOve(CHEAT,CHEAT);
-        int [] scores = player.getScoreBoard();
+        ruleEngine.calculateScoreBasedOnMOve(PlayerMove.COOPERATE,PlayerMove.COOPERATE);
+        ruleEngine.calculateScoreBasedOnMOve(PlayerMove.COOPERATE,PlayerMove.CHEAT);
+        ruleEngine.calculateScoreBasedOnMOve(PlayerMove.CHEAT,PlayerMove.COOPERATE);
+        ruleEngine.calculateScoreBasedOnMOve(PlayerMove.CHEAT,PlayerMove.CHEAT);
+        int [] scores = ruleEngine.getScoreBoard();
         assertEquals(4,scores[0]);
         assertEquals(4,scores[1]);
     }
