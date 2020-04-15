56ea35b3fc9289475e9049c269f1446740c01b49
Priya Virmani <priyavirmani@Priyas-MacBook-Pro.local>
2020-04-15 10:09:16 +0530

    Refactor changes

--- EvolutionOfTrust/src/test/java/PlayerTest.java
+++ EvolutionOfTrust/src/test/java/PlayerTest.java
@@ -5,10 +5,15 @@ import static org.junit.Assert.assertEquals;
 public class PlayerTest {
     @Test
     public void shouldReturnCooperateAlways() {
-
         PlayerMove playerMove = new Player(PlayerMove.COOPERATE).getMove();
 
         assertEquals(PlayerMove.COOPERATE, playerMove);
+    }
+    @Test
+    public void shouldReturnCheatAlways() {
+        PlayerMove playerMove = new Player(PlayerMove.CHEAT).getMove();
+
+        assertEquals(PlayerMove.CHEAT, playerMove);
     }
 
 
